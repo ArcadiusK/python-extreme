2f1734c029cbd4615c17de8b71f1ed9a5e1d04c2
Arcadius Kazimierski Javascript, Node.js, MongoDB, Angular.js, Express.js, C# .Net, SQL Server, Java, Android <arcadiusk@gmail.com>
2015-06-26 08:49:19 -0400

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('this is arcadius!')
+        self.response.write('text/plain response from server')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
